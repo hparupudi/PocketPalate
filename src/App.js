500ef56d74223c42ac03a5801922bc93d63283fa
hparupudi <119982677+hparupudi@users.noreply.github.com>
2024-07-21 13:53:29 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="bg-slate-100 min-h-screen flex flex-col">
+      <header>
+        <h1 className="text-3xl font-bold underline"> Your Personalized </h1>
+        <h1> AI Chef </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
